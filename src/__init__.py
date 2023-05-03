51846daf9b8a75a9e221bd0f4cd5433a22adae9a
HuynhThanhQuan <hthquan28@gmail.com>
2023-05-03 18:27:14 +0700

    Update cred

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,11 @@ PROJECT_LOCATION = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # Set initial environment variables
 os.environ['AICV'] = PROJECT_LOCATION
 
+# Import necessary packages
+import credential
+
 
 if __name__=='__main__':
     print(os.environ.get('AICV'))
+    print(os.environ['AICV_GDRIVE_CRED'])
+    print(os.environ['AICV_GDRIVE_TOKEN'])
