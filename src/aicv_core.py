1e3b7d366b9c214a97a1493ec834ea455c111623
HuynhThanhQuan <hthquan28@gmail.com>
2023-05-08 21:35:53 +0700

    Add customer lifetime calculation

--- src/aicv_core.py
+++ src/aicv_core.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from datetime import datetime
 import capital
+import customer
 
 
 class SecurityCompanyNotImplementedError(Exception):
     pass
-
 
 class BaseAnalyzer:
     def __init__(self):
@@ -25,8 +25,15 @@ class TCBSAnalyzer(BaseAnalyzer):
         table['Ngày GD'] = pd.to_datetime(table['Ngày GD'], format='%Y-%m-%d')
         table['Số hiệu lệnh'] = table['Số hiệu lệnh'].astype(str)
         # Group customers
-        customers_data = table.groupby('Khách hàng')
-        return table
+        customer_data_groups = table.groupby('Khách hàng')
+        return self.analyze_per_customer(customer_data_groups)
+
+    def analyze_per_customer(self, customer_data_groups):
+        for customer_name, customer_txn_data in customer_data_groups:
+            print(customer_name)
+            customer_capital = self.capital.get_deposit_data(customer_name)
+            # Analyze CustomerLifetime
+            customer.CustomerLifetime(customer_name, customer_capital, customer_txn_data)
 
 
 def get_analyzer(security:str) -> BaseAnalyzer:
---
+++ src/customer.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+class CustomerCapitalInvalidError(Exception):
+    pass
+
+
+class CustomerTransactionHistoryInvalidError(Exception):
+    pass
+
+
+class CustomerLifetime:
+    def __init__(self, full_name, capital_history, transaction_history):
+        self.full_name = full_name
+        self.capital_history=capital_history
+        self.transaction_history=transaction_history
+        self.analyze()
+
+    def analyze(self):
+        if self.capital_history is None or len(self.capital_history) == 0:
+            raise CustomerCapitalInvalidError
+        if self.transaction_history is None or len(self.transaction_history) == 0:
+            raise CustomerTransactionHistoryInvalidError
+        
+        # Get first deposit time
+        col = 'Order Time'
+        self.first_deposit_date = self.capital_history[self.capital_history[col]==self.capital_history[col].min()].iloc[0][col]
+        # Get first transaction time
+        col = 'Ngày GD'
+        self.first_txn_date = self.transaction_history[self.transaction_history[col]==self.transaction_history[col].min()].iloc[0][col]
+        self.last_txn_date = self.transaction_history[self.transaction_history[col]==self.transaction_history[col].max()].iloc[0][col]
+        # Get curr date
+        self.current_system_date = datetime.now()
+
+        """
+        There are important milestones of each customer is illustated below
+        Total assets is depended on each duration
+
+        1. first_deposit_date 
+        |       total-asset = cash (100%)
+        2. first_txn_date
+        |       total-asset = cash + stock-assets
+        3. last_txn_date
+        |       total-asset = cash + stock-assets
+        4. current_system_date
+        """
+        self.lifetime = pd.DataFrame()
+        self.lifetime['date'] = pd.date_range(start=self.first_deposit_date, end=self.current_system_date, freq='D')
+        self.lifetime['year'] = self.lifetime['date'].dt.year
+        self.lifetime['month'] = self.lifetime['date'].dt.month
+        self.lifetime['day'] = self.lifetime['date'].dt.day
+        self.lifetime['weekday'] = self.lifetime['date'].dt.weekday
+        self.lifetime['quarter'] = self.lifetime['date'].dt.quarter
+        
+        # Spread capital information in detail
+        self.capital_detail = self.lifetime.copy()
+        self.capital_detail = self.capital_detail.merge(self.capital_history[['Order Time', 'Type', 'Amount']], how='left', left_on='date', right_on='Order Time')
+        self.capital_detail = self.capital_detail.drop(columns=['Order Time'])
+        print(self.capital_detail)
